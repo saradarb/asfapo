fb000dfc1ec8f107d7f434f871e0d91356675b4b
sara <saradernouni@gmail.com>
2025-06-27 12:17:54 -0400

    1

---
+++ src/sections/Bbbutton.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { ButtonContent, Button, Icon } from 'semantic-ui-react'
+
+function Bbbutton() {
+  return (
+    <div>
+         <Button animated>
+      <ButtonContent visible>Next</ButtonContent>
+      <ButtonContent hidden>
+        <Icon name='arrow right' />
+      </ButtonContent>
+    </Button>
+    <Button animated='vertical'>
+      <ButtonContent hidden>Shop</ButtonContent>
+      <ButtonContent visible>
+        <Icon name='shop' />
+      </ButtonContent>
+    </Button>
+    <Button animated='fade'>
+      <ButtonContent visible>Sign-up for a Pro account</ButtonContent>
+      <ButtonContent hidden>$12.99 a month</ButtonContent>
+    </Button>
+    </div>
+  )
+}
+
+export default Bbbutton
